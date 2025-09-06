b9abe05bed3f7b8e7c759c8ca2a0dc83d4882421
aldinioh <aldin0505@gmail.com>
2025-09-07 08:34:05 +1000

    Refactor styles and add new components
    - Imported global styles into style.css for better organization.
    - Added a new SVG logo file for the workspace.
    - Created a new components.css file to define icon sizes and navbar styles.
    - Introduced a globals.css file to manage CSS variables for colors, fonts, and other styles.
    - Added new SVG icons for magnifying glass and shooting star.

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
+<?php
+    require_once 'navbar.php';
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Workspace</title>
     <link rel="stylesheet" href="style.css">
+    
+    <!-- FOR SOME REASON I HAVE TO DO IT LIKE THIS, EVERY OTHER METOD OF STYLING DOES NOT WORK -->
+    <link rel="stylesheet" href="./style/components.css"> 
 </head>
 <body>
+    <?php echo createNavbar() ?>
+
     <section></section>
 
     <div class="effect-background-container">
