8ddf121f11977f3fe9b7ebef7c58c256cde1c737
aldinioh <aldin0505@gmail.com>
2025-09-06 21:06:22 +1000

    Add index.php HTML structure, navbar function, CSS styles, and background video effect

---
+++ index.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Workspace</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section></section>
+
+    <div class="effect-background-container">
+        <video class="effect-background" src="./style/assets/effect.mp4" loop type="video/mp4" preload="auto" autoplay></video>
+    </div>
+</html>
---
+++ navbar.php
@@ -0,0 +1,6 @@
+<?php
+    // Since there is only one style of navbar for assgn1, this code here is very un-reusable, unlike other components with customisable settings
+
+    function createNavbar() {
+    }
+?>
